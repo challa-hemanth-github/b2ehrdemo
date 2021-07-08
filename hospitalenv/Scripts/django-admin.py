f798fc0b8b53d8225770ed23c142a7a14993c7c2
Challa <hemasonofcsrgk1998@gmail.com>
2021-07-08 16:29:32 +0530

    inotial

---
+++ hospitalenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!g:\web\django based\lazycoder hospital management\hospitalmanagement-master\hospitalenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
